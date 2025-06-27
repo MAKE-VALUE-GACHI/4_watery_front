43aa29ae5757c1b8b205b3f08c455776119a3ded
rhctmxk <rhctmxk@naver.com>
2025-06-27 09:42:54 +0900

    [Chore] Config ESLint, Prettier, Babel & 환경 및 스타일 설정

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "media", // 필요 시 활성화
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ export default {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
-    "./hooks/**/*.{js,jsx,ts,tsx}"
+    "./hooks/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
