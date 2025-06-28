3bdd5fc4dc0ca84573c14e86e4726974cce8b68c
minseobshim <mshim@blast-team.com>
2025-06-28 10:59:18 +0900

    [Fix]WaveView 기본 설정

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,52 @@
-import { Image } from "expo-image";
-import "nativewind";
-import { Platform, StyleSheet, Text, View } from "react-native";
-
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { Image } from "expo-image";
+import "nativewind";
+import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet, TouchableHighlight, View } from "react-native";
+import WaveView from "react-native-waveview";
+
+const _styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginVertical: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: StyleSheet.hairlineWidth,
+  },
+  wave: {
+    width: 200,
+    aspectRatio: 1,
+    overflow: "hidden",
+    backgroundColor: "white",
+  },
+});
 
 export default function HomeScreen() {
+  const waveRef = useRef<WaveView>(null);
+  const [testWave, setTestWave] = useState(false);
+
+  useEffect(() => {
+    if (testWave) {
+      waveRef.current?.setWaterHeight(70);
+      waveRef.current?.setWaveParams([
+        { A: 10, T: 180, fill: "#FF9F2E" },
+        { A: 15, T: 140, fill: "#F08200" },
+        { A: 20, T: 100, fill: "#B36100" },
+      ]);
+    } else {
+      // 원래 높이로 되돌리고 애니메이션 재시작
+      waveRef.current?.setWaterHeight(30);
+      waveRef.current?.setWaveParams([
+        { A: 10, T: 180, fill: "#62c2ff" },
+        { A: 15, T: 140, fill: "#0087dc" },
+        { A: 20, T: 100, fill: "#1aa7ff" },
+      ]);
+    }
+  }, [testWave]);
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -22,44 +61,25 @@ export default function HomeScreen() {
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
-      <View>
-        <Text className={"text-red-800"}>Hello</Text>
+      <View style={_styles.container}>
+        <TouchableHighlight
+          onPress={() => {
+            setTestWave(!testWave);
+          }}
+        >
+          <WaveView
+            ref={waveRef}
+            style={_styles.wave}
+            H={30}
+            waveParams={[
+              { A: 10, T: 180, fill: "#62c2ff" },
+              { A: 15, T: 140, fill: "#0087dc" },
+              { A: 20, T: 100, fill: "#1aa7ff" },
+            ]}
+            animated={true}
+          />
+        </TouchableHighlight>
       </View>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: "cmd + d",
-              android: "cmd + m",
-              web: "F12",
-            })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">
-            npm run reset-project
-          </ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
     </ParallaxScrollView>
   );
 }
@@ -69,10 +89,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
   },
   reactLogo: {
     height: 178,
