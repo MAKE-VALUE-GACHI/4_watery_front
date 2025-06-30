8e30ee4bc9acf957997b00af897333f8950f8754
minseobshim <mshim@blast-team.com>
2025-07-01 00:33:08 +0900

    [Feat]Typography 추가

---
+++ components/styles/TextPresets.ts
@@ -0,0 +1,89 @@
+export const presetStyles = {
+  T1: {
+    fontSize: 36,
+    lineHeight: 48,
+    letterSpacing: -2.7,
+    fontWeight: "bold" as const,
+  },
+  T2: {
+    fontSize: 28,
+    lineHeight: 38,
+    letterSpacing: -2.36,
+    fontWeight: "bold" as const,
+  },
+  T3: {
+    fontSize: 24,
+    lineHeight: 32,
+    letterSpacing: -2.3,
+    fontWeight: "bold" as const,
+  },
+  H1: {
+    fontSize: 22,
+    lineHeight: 30,
+    letterSpacing: -1.94,
+    fontWeight: "bold" as const,
+  },
+  H2: {
+    fontSize: 20,
+    lineHeight: 28,
+    letterSpacing: -1.2,
+    fontWeight: "bold" as const,
+  },
+  HL1: {
+    fontSize: 18,
+    lineHeight: 26,
+    letterSpacing: -0.02,
+    fontWeight: "bold" as const,
+  },
+  HL2: {
+    fontSize: 17,
+    lineHeight: 24,
+    letterSpacing: 0,
+    fontWeight: "bold" as const,
+  },
+  BN1: {
+    fontSize: 16,
+    lineHeight: 24,
+    letterSpacing: 0.57,
+  },
+  BR1: {
+    fontSize: 16,
+    lineHeight: 26,
+    letterSpacing: 0.57,
+  },
+  BN2: {
+    fontSize: 15,
+    lineHeight: 22,
+    letterSpacing: 0.96,
+  },
+  BR2: {
+    fontSize: 15,
+    lineHeight: 24,
+    letterSpacing: 0.96,
+  },
+  LN1: {
+    fontSize: 14,
+    lineHeight: 20,
+    letterSpacing: 1.45,
+  },
+  LR1: {
+    fontSize: 14,
+    lineHeight: 22,
+    letterSpacing: 1.45,
+  },
+  L2: {
+    fontSize: 13,
+    lineHeight: 18,
+    letterSpacing: 1.94,
+  },
+  C1: {
+    fontSize: 12,
+    lineHeight: 16,
+    letterSpacing: 2.52,
+  },
+  C2: {
+    fontSize: 11,
+    lineHeight: 14,
+    letterSpacing: 3.11,
+  },
+};
