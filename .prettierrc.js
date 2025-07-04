85a0f23da0c0a8fc0874b4aaa13a4ebe6830f886
rhctmxk <rhctmxk@naver.com>
2025-07-04 13:23:52 +0900

    [Chore] Prettier 파일명 & 스타일 업데이트

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  singleQuote: true,
+  singleQuote: false,
   trailingComma: "all",
   tabWidth: 2,
   semi: true,
   printWidth: 100,
+  parser: "babel",
+  bracketSpacing: true,
+  endOfLine: "lf",
 };
