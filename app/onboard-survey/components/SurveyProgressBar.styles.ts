0af62c17819c9e24e172a36a535d12851c1025ce
minseobshim <mshim@blast-team.com>
2025-07-15 17:17:13 +0900

    [Feat]ProgressBar 구현

---
+++ app/onboard-survey/components/SurveyProgressBar.styles.ts
@@ -0,0 +1,19 @@
+import { ThemedView } from "@/components/ThemedView";
+import { Colors } from "@/constants/Colors";
+import styled from "styled-components/native";
+
+export const ProgressBarWrapper = styled(ThemedView)`
+  flex-direction: row;
+  padding: 0 20px;
+  gap: 8px;
+  margin-bottom: 34px;
+`;
+
+export const ProgressBar = styled(ThemedView)<{ isActive: boolean }>`
+  height: 5px;
+  border-radius: 7px;
+  flex: 1;
+  background-color: ${(props) => (props.isActive ? Colors.primary_800 : Colors.primary_050)};
+`;
+
+export default ProgressBar;
--- app/onboard-survey/index.tsx
+++ app/onboard-survey/index.tsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import { ThemedView } from "@/components/ThemedView";
 import SurveyHeader from "@/app/onboard-survey/components/SurveyHeader";
+import SurveyProgressBar from "@/app/onboard-survey/components/SurveyProgressBar";
 
 const categories = ["성별", "출생연도", "체중", "하루 평균 활동량"];
 
 const OnboardSurvey = () => {
-  const [surveyStep, setSurveyStep] = useState(0);
+  const [currentStep, setCurrentStep] = useState(1);
 
   return (
     <ThemedView>
-      <SurveyHeader currentStep={surveyStep} totalSteps={4} categoryName={categories[surveyStep]} />
+      <SurveyProgressBar currentStep={currentStep} totalSteps={4} />
+      <SurveyHeader
+        currentStep={currentStep}
+        totalSteps={4}
+        categoryName={categories[currentStep]}
+      />
     </ThemedView>
   );
 };
