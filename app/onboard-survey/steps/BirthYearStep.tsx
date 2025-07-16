5b5fbe0b1e3bfb8a7f5325716f452313c90b8ecd
minseobshim <mshim@blast-team.com>
2025-07-17 02:04:22 +0900

    [Feat]BirthYearStep 구현

--- app/onboard-survey/steps/BirthYearStep.tsx
+++ app/onboard-survey/steps/BirthYearStep.tsx
@@ -1,10 +1,37 @@
-import { H1 } from "@/components/ThemedText";
 import { StepContainer } from "@/app/onboard-survey/steps/steps.styles";
+import { Colors } from "@/constants/Colors";
+import { useEffect, useState } from "react";
+import { useFormContext } from "react-hook-form";
+import WheelPicker from "react-native-wheel-picker-expo";
+
+const years = Array.from({ length: 100 }, (_, i) => 2025 - i);
 
 const BirthYearStep = () => {
+  const { setValue, watch } = useFormContext();
+  const birthYear = watch("birthYear");
+  const [selectedIndex, setSelectedIndex] = useState(
+    birthYear ? years.indexOf(Number(birthYear)) : 0,
+  );
+
+  useEffect(() => {
+    if (birthYear) {
+      setSelectedIndex(years.indexOf(Number(birthYear)));
+    }
+  }, [birthYear]);
+
   return (
     <StepContainer>
-      <H1>BirthYear</H1>
+      <WheelPicker
+        height={300}
+        width={150}
+        initialSelectedIndex={selectedIndex}
+        selectedStyle={{ borderColor: Colors.neutral_200, borderWidth: 1 }}
+        items={years.map((name) => ({ label: String(name), value: String(name) }))}
+        onChange={({ index, item }) => {
+          setSelectedIndex(index);
+          setValue("birthYear", item.label);
+        }}
+      />
     </StepContainer>
   );
 };
--- app/onboard-survey/steps/steps.styles.ts
+++ app/onboard-survey/steps/steps.styles.ts
@@ -4,6 +4,7 @@ import { ThemedView } from "@/components/ThemedView";
 export const StepContainer = styled(ThemedView)`
   flex: 1;
   padding-top: 48px;
+  align-items: center;
   gap: 16px;
 `;
 
