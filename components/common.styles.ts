c3be2e88375d8c1f7b29752c22363742a8cfd3cd
minseobshim <mshim@blast-team.com>
2025-07-17 15:49:08 +0900

    [Feat]FlexRow 공용 컴포넌트에 align-items, justify-content 속성 추가

--- components/common.styles.ts
+++ components/common.styles.ts
@@ -1,5 +1,17 @@
-import styled from "styled-components/native";
 import { ThemedView } from "@/components/ThemedView";
+import styled from "styled-components/native";
+
+interface FlexRowProps {
+  gap?: number;
+  alignItems?: "flex-start" | "center" | "flex-end" | "stretch" | "baseline";
+  justifyContent?:
+    | "flex-start"
+    | "center"
+    | "flex-end"
+    | "space-between"
+    | "space-around"
+    | "space-evenly";
+}
 
 export const TransparentView = styled(ThemedView)`
   background-color: transparent;
@@ -10,7 +22,11 @@ export const TransparentFlexRow = styled(TransparentView)<{ gap?: number }>`
   ${({ gap }: { gap: number }) => gap && `gap: ${gap}px;`}
 `;
 
-export const FlexRow = styled(ThemedView)<{ gap?: number }>`
+export const FlexRow = styled(ThemedView)<FlexRowProps>`
   flex-direction: row;
+  align-items: ${({ alignItems }: { alignItems: FlexRowProps["alignItems"] }) =>
+    alignItems || "flex-start"};
+  justify-content: ${({ justifyContent }: { justifyContent: FlexRowProps["justifyContent"] }) =>
+    justifyContent || "flex-start"};
   ${({ gap }: { gap: number }) => gap && `gap: ${gap}px;`}
 `;
