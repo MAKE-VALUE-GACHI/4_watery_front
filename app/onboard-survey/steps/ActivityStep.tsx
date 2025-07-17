0a60366d47aee19b936c1bf5e72113cb3a5707c6
minseobshim <mshim@blast-team.com>
2025-07-17 16:15:39 +0900

    [Feat]활동량 단계 추가

--- app/onboard-survey/steps/ActivityStep.tsx
+++ app/onboard-survey/steps/ActivityStep.tsx
@@ -1,10 +1,58 @@
-import { H1 } from "@/components/ThemedText";
 import { StepContainer } from "@/app/onboard-survey/steps/steps.styles";
+import { useFormContext } from "react-hook-form";
+import { useEffect, useState } from "react";
+import SurveyOption from "@/components/SurveyOption/SurveyOption";
+
+const activityOptions = ["low", "normal", "active", "very_active"];
 
 const ActivityStep = () => {
+  const { setValue, watch } = useFormContext();
+  const activity = watch("activity");
+  const [selectedOptionIdx, setSelectedOptionIdx] = useState(
+    activity ? activityOptions.indexOf(activity) + 1 : 1,
+  );
+
+  useEffect(() => {
+    if (activity) {
+      setSelectedOptionIdx(activityOptions.indexOf(activity) + 1);
+    }
+  }, [activity]);
+
+  const optionToString = (option: string) => {
+    switch (option) {
+      case "low":
+        return "거의 운동하지 않아요";
+
+      case "normal":
+        return "가볍게 움직이는 편이에요";
+
+      case "active":
+        return "일주일에 몇 번은 운동해요";
+
+      case "very_active":
+        return "메일 운동하거나 활동량이 많아요";
+
+      default:
+        return "가볍게 움직이는 편이에요";
+    }
+  };
+
+  const changeSelectedOption = (idx: number) => {
+    setSelectedOptionIdx(idx);
+    setValue("activity", activityOptions[idx - 1]);
+  };
+
   return (
     <StepContainer>
-      <H1>Activity</H1>
+      {activityOptions.map((option, idx) => (
+        <SurveyOption
+          key={`activity_option_${idx}`}
+          title={optionToString(option)}
+          isSelected={selectedOptionIdx === idx + 1}
+          optionIdx={idx + 1}
+          onPress={() => changeSelectedOption(idx + 1)}
+        />
+      ))}
     </StepContainer>
   );
 };
