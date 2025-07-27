5f6b090e632769d9f5a75efaf063cef663a8fc16
minseobshim <mshim@blast-team.com>
2025-07-27 18:19:43 +0900

    [Refactor]코드 추상화 및 정리

---
+++ app/(tabs)/const/beverageDataSet.ts
@@ -0,0 +1,11 @@
+import { BeverageVariantType } from "@/components/CustomSlide/CustomSlide.types";
+
+const beverageDataSet: (BeverageVariantType | "addYourself")[] = [
+  "water",
+  "coffee",
+  "greenTea",
+  "cola",
+  "addYourself",
+];
+
+export default beverageDataSet;
---
+++ app/(tabs)/utils/renderItem.tsx
@@ -0,0 +1,31 @@
+import AddYourselfSlide from "@/app/(tabs)/components/AddYourselfSlide";
+import beverageDataSet from "@/app/(tabs)/const/beverageDataSet";
+import CustomSlide from "@/components/CustomSlide/CustomSlide";
+import { BeverageVariantType } from "@/components/CustomSlide/CustomSlide.types";
+import React from "react";
+import { ImageStyle, StyleProp } from "react-native";
+import { CarouselRenderItem } from "react-native-reanimated-carousel";
+
+interface renderItemProps {
+  style?: StyleProp<ImageStyle>;
+}
+
+const renderItem = (options: renderItemProps = {}): CarouselRenderItem<any> => {
+  return ({ index }) => {
+    const redirectAddYourself = () => {
+      console.log("redirecting to Add Yourself Page . . .");
+    };
+
+    return beverageDataSet[index] === "addYourself" ? (
+      <AddYourselfSlide key={index} style={options.style} onPress={redirectAddYourself} />
+    ) : (
+      <CustomSlide
+        key={index}
+        beverageVariant={beverageDataSet[index] as BeverageVariantType}
+        style={options.style}
+      />
+    );
+  };
+};
+
+export default renderItem;
