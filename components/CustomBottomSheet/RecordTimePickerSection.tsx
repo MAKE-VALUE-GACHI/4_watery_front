13b29fa6e82e9ccb3677792385ef1fa5497227f9
minseobshim <mshim@blast-team.com>
2025-08-03 23:45:49 +0900

    [Feat]중복 코드 정리 및 스타일 조정, 시간 사이에 : 추가

--- components/CustomBottomSheet/RecordTimePickerSection.tsx
+++ components/CustomBottomSheet/RecordTimePickerSection.tsx
@@ -1,10 +1,10 @@
-import { Colors } from "@/constants/Colors";
 import { horizontalScale, verticalScale } from "@/libs/utils/scaling";
 import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import WheelPicker from "react-native-wheel-picker-expo";
 import { combinePickerToDate, splitDateToPicker } from "@/hooks/useDateTimeTransform";
 import { recordTimePickerSectionStyles } from "@/components/CustomBottomSheet/RecordTimePickerSection.styles";
+import { HL1 } from "@/components/ThemedText";
 
 interface RecordTimePickerSectionProps {
   value: Date;
@@ -20,7 +20,7 @@ const RecordTimePickerSection: React.FC<RecordTimePickerSectionProps> = ({ value
     label: String(i + 1),
     value: String(i + 1),
   }));
-  const minuteOptions = Array.from({ length: 60 }, (_, i) => ({
+  const minuteOptions = Array.from({ length: 65 }, (_, i) => ({
     label: i.toString().padStart(2, "0"),
     value: String(i),
   }));
@@ -46,25 +46,29 @@ const RecordTimePickerSection: React.FC<RecordTimePickerSectionProps> = ({ value
     <View style={recordTimePickerSectionStyles.timePickerWrapper}>
       <WheelPicker
         height={verticalScale(172)}
-        width={horizontalScale(75)}
+        width={horizontalScale(65)}
         items={ampmOptions}
-        selectedStyle={{ borderColor: Colors.neutral_200, borderWidth: 1 }}
+        selectedStyle={recordTimePickerSectionStyles.wheelPickerSelectedItem}
         initialSelectedIndex={ampm === "AM" ? 0 : 1}
         onChange={({ item }) => setAmpm(item.value)}
       />
+
       <WheelPicker
         height={verticalScale(172)}
-        width={horizontalScale(75)}
+        width={horizontalScale(65)}
         items={hourOptions}
-        selectedStyle={{ borderColor: Colors.neutral_200, borderWidth: 1 }}
+        selectedStyle={recordTimePickerSectionStyles.wheelPickerSelectedItem}
         initialSelectedIndex={Number(hour) - 1}
         onChange={({ item }) => setHour(item.value)}
       />
+
+      <HL1 style={recordTimePickerSectionStyles.timeDot}>:</HL1>
+
       <WheelPicker
         height={verticalScale(172)}
-        width={horizontalScale(75)}
+        width={horizontalScale(65)}
         items={minuteOptions}
-        selectedStyle={{ borderColor: Colors.neutral_200, borderWidth: 1 }}
+        selectedStyle={recordTimePickerSectionStyles.wheelPickerSelectedItem}
         initialSelectedIndex={Number(minute)}
         onChange={({ item }) => setMinute(item.value)}
       />
